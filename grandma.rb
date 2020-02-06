15003da4764d0aac26b65d90fbba7eeb3849ad8f
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-06 19:11:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma (input)
+def speak_to_grandma(input)
   if input == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif input == input.upcase
