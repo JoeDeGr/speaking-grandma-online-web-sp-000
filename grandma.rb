2a28641d895d1078eb079da0b676802bf26ea85e
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-06 19:13:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(input)
   elsif input == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
-    puts "NO, NOT SINCE 1938!"  
+    puts "NO, NOT SINCE 1938!"
   end
 end
 
