0973a5dd0663eaa9492beac264e1a2d590890622
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-06 19:13:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(input)
-  if input == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  elsif input == input.upcase
-    puts "NO, NOT SINCE 1938!"
-  else
+  if input !== input.upcase
     puts "HUH?! SPEAK UP, SONNY!"
+  elsif input == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else
+    puts "NO, NOT SINCE 1938!"  
   end
 end
 
